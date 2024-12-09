e467dab68b0e72180ead580c0730776ec1e18211
Amritanshu Kumar <amritanshu8271@gmail.com>
2024-12-09 16:13:47 +0530

    Create contact added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,48 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        AddressBook a1= new AddressBook();
+        int i=1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Address book");
+        do{
+            System.out.println("Enter 1 to create contact");
+            int input = sc.nextInt();
+            switch (input){
+                case 1-> createContact(a1);
+                default -> System.out.println("wrong input");
+            }
+            System.out.println("Press 0 to exit");
+            i= sc.nextInt();
+        }while (i!=0);
+
+        System.out.println("Thank you for using address book");
+        sc.close();
+    }
+
+    static void createContact(AddressBook a1){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter name");
+        String name = sc.next();
+        System.out.println("Enter last name");
+        String lname = sc.next();
+        System.out.println("Enter city");
+        String city = sc.next();
+        System.out.println("Enter state");
+        String state = sc.next();
+        System.out.println("Enter email");
+        String email = sc.next();
+        System.out.println("Enter phone");
+        int phone = sc.nextInt();
+        System.out.println("Enter zip");
+        int zip = sc.nextInt();
+        Contact c1 = new Contact(name,lname,city,state,email,phone,zip);
+        a1.addContact(c1);
+
 
     }
 }
